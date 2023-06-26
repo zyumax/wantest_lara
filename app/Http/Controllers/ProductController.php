347c1c7ace8d10f5ef10cec6ca7273e5835bc55a
zyumax <kazuma_sge@icloud.com>
2023-06-27 07:28:34 +0900

    productControllerの作成

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index() {
+        $products = Product::all();
+        return view('index', compact('products'));
+    }
+
+    public function store(Request $request) {
+        $product = Product::create(
+            [
+            'name' => $request->title,
+            'price' => $request->price,
+            'remark' => $request->remark,
+            'evaluation' => $request->evaluation
+            ]
+        );
+    }
+
+}
