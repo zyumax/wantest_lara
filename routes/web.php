55ccec4a178bcadb07b59b33b00f3c815b3fe96f
zyumax <kazuma_sge@icloud.com>
2023-07-10 23:18:04 +0900

    編集機能追加

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/', [ProductController::class, 'index'])->name('product.index');
 Route::get('create', [ProductController::class, 'create'])->name('product.create');
 Route::post('post', [ProductController::class, 'store'])->name('product.store');
 Route::post('post/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
+Route::get('show', [ProductController::class, 'show'])->name('product.show');
+Route::get('post/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
+Route::patch('product/{product}', [ProductController::class, 'update'])->name('product.update');
